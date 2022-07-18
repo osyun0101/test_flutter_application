286243918378c22009232d6d6e14c337a88dceaa
natyudon <61405828+natyudon@users.noreply.github.com>
2022-07-18 22:56:35 +0900

    fix: ConstrainedBoxで明示的に高さ上限を指定していた箇所はデフォルトの状態と変わらず無駄な設定だったため削除

--- lib/main.dart
+++ lib/main.dart
@@ -55,42 +55,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-        constraints:
-            BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text(widget.title),
-              backgroundColor: Theme.of(context).backgroundColor,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Theme.of(context).backgroundColor,
+        ),
+        body: Center(
+          child: Column(children: [
+            const Text(
+              'U L U',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 50),
             ),
-            body: Center(
-              child: Column(children: [
-                const Text(
-                  'U L U',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 50),
-                ),
-                Image.asset('images/biyouin_sample.png'),
-                Container(
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.all(8.0),
-                  color: Theme.of(context).primaryColor,
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 100,
-                    child: ElevatedButton(
-                      onPressed: opneUrl,
-                      child: const Text(
-                        '''
+            Image.asset('images/biyouin_sample.png'),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(8.0),
+              color: Theme.of(context).primaryColor,
+              child: SizedBox(
+                width: double.infinity,
+                height: 100,
+                child: ElevatedButton(
+                  onPressed: opneUrl,
+                  child: const Text(
+                    '''
 ONLINE SHOP
 オンラインショップ
                 ''',
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                /*Expanded(
+              ),
+            ),
+            /*Expanded(
                     child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
@@ -106,49 +103,40 @@ ONLINE SHOP
                             child: Text("green"))),
                   ],
                 ))*/
+            Expanded(
+                child: Row(
+              children: [
                 Expanded(
-                    child: Row(
-                  children: [
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green")),
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green")),
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green"))
-                  ],
-                )),
+                    child: myWidget(Theme.of(context).primaryColor, "green")),
                 Expanded(
-                    child: Row(
-                  children: [
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green")),
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green")),
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green"))
-                  ],
-                )),
+                    child: myWidget(Theme.of(context).primaryColor, "green")),
                 Expanded(
-                    child: Row(
-                  children: [
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green")),
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green")),
-                    Expanded(
-                        child:
-                            myWidget(Theme.of(context).primaryColor, "green"))
-                  ],
-                ))
-                /*mainAxisAlignment: MainAxisAlignment.center,
+                    child: myWidget(Theme.of(context).primaryColor, "green"))
+              ],
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                    child: myWidget(Theme.of(context).primaryColor, "green")),
+                Expanded(
+                    child: myWidget(Theme.of(context).primaryColor, "green")),
+                Expanded(
+                    child: myWidget(Theme.of(context).primaryColor, "green"))
+              ],
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                    child: myWidget(Theme.of(context).primaryColor, "green")),
+                Expanded(
+                    child: myWidget(Theme.of(context).primaryColor, "green")),
+                Expanded(
+                    child: myWidget(Theme.of(context).primaryColor, "green"))
+              ],
+            ))
+            /*mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
@@ -158,41 +146,41 @@ ONLINE SHOP
               style: Theme.of(context).textTheme.headline4,
             ),
           ],*/
-              ]),
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-                selectedLabelStyle: const TextStyle(color: Colors.black),
-                unselectedLabelStyle: const TextStyle(color: Colors.black),
-                showUnselectedLabels: true, //選択されていないタブのラベルも表示する
-                selectedItemColor: Colors.blue,
-                currentIndex: _selectedIndex,
-                onTap: _onItemTapped,
-                type: BottomNavigationBarType
-                    .fixed, //４つ以上だとラベルが非表示になるためこのオプションを指定して解消
-                items: const <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
-                      activeIcon: Icon(Icons.home),
-                      label: 'Home'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
-                      activeIcon: Icon(Icons.home, color: Colors.blue),
-                      label: '予約'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
-                      activeIcon: Icon(Icons.home, color: Colors.blue),
-                      label: '店舗情報'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.person),
-                      activeIcon: Icon(Icons.home, color: Colors.blue),
-                      label: 'マイページ')
-                ])
-            /*floatingActionButton: FloatingActionButton(
+          ]),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            selectedLabelStyle: const TextStyle(color: Colors.black),
+            unselectedLabelStyle: const TextStyle(color: Colors.black),
+            showUnselectedLabels: true, //選択されていないタブのラベルも表示する
+            selectedItemColor: Colors.blue,
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            type: BottomNavigationBarType
+                .fixed, //４つ以上だとラベルが非表示になるためこのオプションを指定して解消
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  activeIcon: Icon(Icons.home),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  activeIcon: Icon(Icons.home, color: Colors.blue),
+                  label: '予約'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  activeIcon: Icon(Icons.home, color: Colors.blue),
+                  label: '店舗情報'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  activeIcon: Icon(Icons.home, color: Colors.blue),
+                  label: 'マイページ')
+            ])
+        /*floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), */ // This trailing comma makes auto-formatting nicer for build methods.
-            ));
+        );
   }
 
   Widget myWidget(Color color, String text) {
