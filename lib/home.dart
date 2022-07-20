d4d636cdf35daca6218ed94de96b2614c0e6304f
natyudon <61405828+natyudon@users.noreply.github.com>
2022-07-20 23:20:16 +0900

    タブ切り替え実装

---
+++ lib/home.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  void opneUrl() async {
+    Uri url = Uri.parse('https://geniepi-hiroba.com/');
+    if (await canLaunchUrl(url)) {
+      //mode: LaunchMode.externalApplicationにするとアプリ内ではなくブラウザを起動して表示するようにできる
+      await launchUrl(url);
+    } else {
+      throw 'このURLにはアクセスできません';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      const Text(
+        'U L U',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 50),
+      ),
+      Image.asset('images/biyouin_sample.png'),
+      Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(8.0),
+        color: Theme.of(context).primaryColor,
+        child: SizedBox(
+          width: double.infinity,
+          height: 100,
+          child: ElevatedButton(
+            onPressed: opneUrl,
+            child: const Text(
+              '''
+ONLINE SHOP
+オンラインショップ
+                ''',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+      /*Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                            alignment: Alignment.center,
+                            color: Theme.of(context).primaryColor,
+                            child: Text("green"))),
+                    Expanded(
+                        child: Container(
+                            alignment: Alignment.center,
+                            color: Theme.of(context).primaryColor,
+                            child: Text("green"))),
+                  ],
+                ))*/
+      Expanded(
+          child: Row(
+        children: [
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green")),
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green")),
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green"))
+        ],
+      )),
+      Expanded(
+          child: Row(
+        children: [
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green")),
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green")),
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green"))
+        ],
+      )),
+      Expanded(
+          child: Row(
+        children: [
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green")),
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green")),
+          Expanded(child: myWidget(Theme.of(context).primaryColor, "green"))
+        ],
+      ))
+    ]);
+  }
+
+  Widget myWidget(Color color, String text) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        alignment: Alignment.center,
+        color: color,
+        child: Text(text),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_flutter_application/home.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -30,13 +31,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
-  //int _counter = 0;
 
-  /*void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }*/
+  // 表示する Widget の一覧
+  static final List<Widget> _pageList = [
+    const Home(),
+    const Text("2tab")
+  ];
+
   void opneUrl() async {
     Uri url = Uri.parse('https://geniepi-hiroba.com/');
     if (await canLaunchUrl(url)) {
@@ -61,92 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).backgroundColor,
         ),
         body: Center(
-          child: Column(children: [
-            const Text(
-              'U L U',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 50),
-            ),
-            Image.asset('images/biyouin_sample.png'),
-            Container(
-              alignment: Alignment.center,
-              margin: const EdgeInsets.all(8.0),
-              color: Theme.of(context).primaryColor,
-              child: SizedBox(
-                width: double.infinity,
-                height: 100,
-                child: ElevatedButton(
-                  onPressed: opneUrl,
-                  child: const Text(
-                    '''
-ONLINE SHOP
-オンラインショップ
-                ''',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            /*Expanded(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                        child: Container(
-                            alignment: Alignment.center,
-                            color: Theme.of(context).primaryColor,
-                            child: Text("green"))),
-                    Expanded(
-                        child: Container(
-                            alignment: Alignment.center,
-                            color: Theme.of(context).primaryColor,
-                            child: Text("green"))),
-                  ],
-                ))*/
-            Expanded(
-                child: Row(
-              children: [
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green")),
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green")),
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green"))
-              ],
-            )),
-            Expanded(
-                child: Row(
-              children: [
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green")),
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green")),
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green"))
-              ],
-            )),
-            Expanded(
-                child: Row(
-              children: [
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green")),
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green")),
-                Expanded(
-                    child: myWidget(Theme.of(context).primaryColor, "green"))
-              ],
-            ))
-            /*mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],*/
-          ]),
+          child: _pageList[_selectedIndex],
         ),
         bottomNavigationBar: BottomNavigationBar(
             selectedLabelStyle: const TextStyle(color: Colors.black),
@@ -175,11 +91,6 @@ ONLINE SHOP
                   activeIcon: Icon(Icons.home, color: Colors.blue),
                   label: 'マイページ')
             ])
-        /*floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), */ // This trailing comma makes auto-formatting nicer for build methods.
         );
   }
 
